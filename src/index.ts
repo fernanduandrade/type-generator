1a4cdb866e390306ebb882f29b416f5a5d32d062
fernanduandrade <onanduandrade@gmail.com>
2022-11-05 09:51:21 -0300

    add: added prompt to ask user the type name and the json url

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import axios from 'axios'
 import { jsonToPrimitive } from './parser'
-
+import Prompt from 'prompt-sync'
 export const ProvideType =  async (baseUrl: string) => {
   const response = axios(`${baseUrl}`, {});
   const newObj = {}
   return jsonToPrimitive((await response).data, newObj)
 }
 
-ProvideType('https://jsonplaceholder.typicode.com/users').then(res => {
-  console.log(`type Users = ${JSON.stringify(res, null, 2).replace(/\"/g, "")}`)
+const prompt = Prompt()
+
+const entityPrompt = prompt(`enter with the entity name: `)
+const uriPrompt = prompt(`enter with the json url: `)
+
+ProvideType(`${uriPrompt}`).then(res => {
+  console.log(`type ${entityPrompt} = ${JSON.stringify(res, null, 2).replace(/\"/g, "")}`)
 })
