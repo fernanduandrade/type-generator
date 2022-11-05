15023d2f6d2b53700c7fa75b60d4d23ffb7da8e6
fernanduandrade <onanduandrade@gmail.com>
2022-11-05 16:10:20 -0300

    fix: fix index main function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import app from './app'
+import GetType from './services/fetch-type'
+import { getUserInputs, copyResultToClipBoard } from './utils/prompt-helper';
 
-app()
+export function fetchType() {
+  const { entityInput, jsonUrlInput } = getUserInputs()
+
+  GetType(`${jsonUrlInput}`).then(res => {
+    const resultStringify = JSON.stringify(res, null, 2).replace(/\"/g, "")
+    const output = `type ${entityInput} = ${resultStringify}`
+    console.log(output)
+    copyResultToClipBoard(output)
+  })
+}
+
+fetchType()
